7e5fff29ab8760ba507bb2f20152c141ac351946
Manikandan1142 <manieee1126@gmail.com>
2025-09-22 11:44:44 +0530

    font check delete

--- checkFonts.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require("child_process");
-
-try {
-    const fonts = execSync("fc-list : family", { encoding: "utf8" });
-    console.log("Available fonts on Railway:\n", fonts);
-} catch (err) {
-    console.error("Error listing fonts:", err.message);
-}
